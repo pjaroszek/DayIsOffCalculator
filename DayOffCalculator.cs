e49851c993114eb197c36920a1667b21be07d8d5
Pawel Jaroszek <pjaroszek@gmail.com>
2020-02-22 21:43:46 +0100

    change 3 king to day off

--- DayOffCalculator.cs
+++ DayOffCalculator.cs
@@ -8,7 +8,7 @@ namespace Jaroszek.ProofOfCOncept.DayIsOffCalculator
             if (date.DayOfWeek == DayOfWeek.Saturday) return true;
             if (date.DayOfWeek == DayOfWeek.Sunday) return true;
             if (date.Month == 01 && date.Day == 01) return true; // Nowy Rok
-            if (date.Month == 01 && date.Day == 06 && (date.Year >= 1952 && date.Year <= 1960)) return true; // Trzech Króli if (date.Month == 05 && date.Day == 01) return true; // 1 maja if (date.Month == 05 && date.Day == 03 && (date.Year >= 1918 && date.Year <= 1950 || date.Year >= 1990)) return true; // 3 maja
+            if (date.Month == 01 && date.Day == 06 && (date.Year >= 1952 && date.Year >= 2015)) return true; // Trzech Króli if (date.Month == 05 && date.Day == 01) return true; // 1 maja if (date.Month == 05 && date.Day == 03 && (date.Year >= 1918 && date.Year <= 1950 || date.Year >= 1990)) return true; // 3 maja
             if (date.Month == 07 && date.Day == 22 && (date.Year >= 1945 && date.Year <= 1989)) return true; // Narodowe Święto Odrodzenia Polski
             if (date.Month == 08 && date.Day == 15 && (date.Year <= 1960 || date.Year >= 1989)) return true; // Wniebowzięcie Najświętszej Marii Panny, Święto Wojska Polskiego (rocznica “cudu nad Wisłą”)
             if (date.Month == 11 && date.Day == 01) return true; // Dzień Wszystkich Świętych
--- Program.cs
+++ Program.cs
@@ -5,13 +5,15 @@
     {
         static void Main(string[] args)
         {
-            var result = DayOffCalculator.IsDayOff(DateTime.Today);
-            Console.WriteLine($"czy dzien pracujący {result}");
+var dateToCheck = DateTime.Parse("2020-01-06");//DateTime.Today);
 
-            var resultPreviusWorkDay = DayOffCalculator.GetNearestPreviousWorkDay(DateTime.Today);
+            var result = DayOffCalculator.IsDayOff(dateToCheck);
+            Console.WriteLine($"czy {dateToCheck} jest dniem wolnym od pracy? -> {result}");
+
+            var resultPreviusWorkDay = DayOffCalculator.GetNearestPreviousWorkDay(dateToCheck);
             Console.WriteLine($"Najbliższy dzień pracujący poprzedzający dzień woly to {resultPreviusWorkDay}");
 
-            var resultNextWorkDay = DayOffCalculator.GetNearestNextWorkDay(DateTime.Today);
+            var resultNextWorkDay = DayOffCalculator.GetNearestNextWorkDay(dateToCheck);
             System.Console.WriteLine($"pierwszy następny dzień pracujący to {resultNextWorkDay}");
 
             Console.ReadKey();
