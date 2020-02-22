9ca3a1fedc4cea3d53a0ba38de9868805041f1bf
Pawel Jaroszek <pjaroszek@gmail.com>
2020-02-22 21:07:34 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DayIsOffCalculator
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DayOffCalculator.IsDayOff(DateTime.Today);
+            Console.WriteLine($"czy dzien pracujący {result}");
+
+            var resultPreviusWorkDay = DayOffCalculator.GetNearestPreviousWorkDay(DateTime.Today);
+            Console.WriteLine($"Najbliższy dzień pracujący poprzedzający dzień woly to {resultPreviusWorkDay}");
+
+            var resultNextWorkDay = DayOffCalculator.GetNearestNextWorkDay(DateTime.Today);
+            System.Console.WriteLine($"pierwszy następny dzień pracujący to {resultNextWorkDay}");
+
+            Console.ReadKey();
+        }
+    }
+}
